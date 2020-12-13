8d64aee47b08ce9762f210ef4dd6c82aa1b0f0b9
jocelyn4112 <68340393+jocelyn4112@users.noreply.github.com>
2020-12-13 15:41:39 -0500

    Getting there

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -1,72 +1,50 @@
 // from data.js
-var tableData = data;
-
-// The new student and grade to add to the table
-??var data = ["Wash", 79];
-
-// Use D3 to select the table
-var table = d3.select("table");
-
-// Use d3 to create a bootstrap striped table
-// http://getbootstrap.com/docs/3.3/css/#tables-striped
-table.attr("class", "table table-zebra striped");
+const tableData = data;
+//console.log(tableData)
 
 // Use D3 to select the table body
-var tbody = d3.select("tbody");
+const tbody = d3.select("tbody");
 
-// Append one table row `tr` to the table body
-var row = tbody.append("tr");
 
-// Append one cell for the student name
-row.append("td").text(newGrade[0]);
+function buildtable(data){
 
-// Append one cell for the student grade
-row.append("td").text(newGrade[1]);
-/ // Step 1: Loop Through `data` and console.log each weather report object
-// data.forEach(function(weatherReport) {
-//   console.log(weatherReport);
-// });
+    tbody.html("");
+    data.forEach(element => {
 
-// // Step 2:  Use d3 to append one table row `tr` for each weather report object
-// // Don't worry about adding cells or text yet, just try appending the `tr` elements.
-// data.forEach(function(weatherReport) {
-//   console.log(weatherReport);
-//   var row = tbody.append("tr");
-// });
+        // Append one table row `tr` to the table body
+        var row = tbody.append("tr");
 
-// // Step 3:  Use `Object.entries` to console.log each weather report value
-// data.forEach(function(weatherReport) {
-//   console.log(weatherReport);
-//   var row = tbody.append("tr");
+        Object.values(element).forEach((val) => {
+            var rowData = row.append("td");
+            rowData.text(val);
 
-//   Object.entries(weatherReport).forEach(function([key, value]) {
-//     console.log(key, value);
-//   });
-// });
+        });
+    });
+}
 
-// // Step 4: Use d3 to append 1 cell per weather report value (weekday, date, high, low)
-// data.forEach(function(weatherReport) {
-//   console.log(weatherReport);
-//   var row = tbody.append("tr");
 
-//   Object.entries(weatherReport).forEach(function([key, value]) {
-//     console.log(key, value);
-//     // Append a cell to the row for each value
-//     // in the weather report object
-//     var cell = row.append("td");
-//   });
-// });
 
-// // Step 5: Use d3 to update each cell's text with
-// // weather report values (weekday, date, high, low)
-// data.forEach(function(weatherReport) {
-//   console.log(weatherReport);
-//   var row = tbody.append("tr");
-//   Object.entries(weatherReport).forEach(function([key, value]) {
-//     console.log(key, value);
-//     // Append a cell to the row for each value
-//     // in the weather report object
-//     var cell = row.append("td");
-//     cell.text(value);
-//   });
-// });
+
+// Make Button
+var button = d3.select("#filter-btn");
+button.on("click", handleClick);
+
+// Grab info we need 
+
+
+
+// This function is triggered when the button is clicked
+function handleClick() {
+
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+    console.log(inputValue);
+
+    buildtable(tableData);
+
+}
+
+buildtable(tableData);
